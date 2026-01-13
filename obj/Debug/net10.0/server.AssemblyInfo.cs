badf7d8c8d1e4367bd06e97f561c91cc655f9c14
allchai <rvachevkir@gmail.com>
2026-01-14 01:36:33 +0700

    added dto

--- obj/Debug/net10.0/server.AssemblyInfo.cs
+++ obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ec021a7a3813c6143a0e28fdc50cca50d06b20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c354dc99bb97b8a47208e52f0261ce3fafd5b55f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
