c354dc99bb97b8a47208e52f0261ce3fafd5b55f
allchai <rvachevkir@gmail.com>
2026-01-14 01:28:34 +0700

    Dot env file and entry point

--- Program.cs
+++ Program.cs
@@ -1,22 +1,40 @@
+using server.Data;
+using server.Middlewares;
+using server.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+DotNetEnv.Env.Load();
+
+var connectionsString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                        $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                        $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                        $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                        $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionsString));
+
+builder.Services.AddScoped<IScheduleServices, SchedulService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
