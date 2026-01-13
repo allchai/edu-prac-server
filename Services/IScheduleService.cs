66bbf1189fe5b7a28d59bc4007d763612c750ee0
allchai <rvachevkir@gmail.com>
2026-01-14 02:07:17 +0700

    Added services

---
+++ Services/IScheduleService.cs
@@ -0,0 +1,9 @@
+﻿using server.Dto;
+
+namespace server.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime startDate, DateTime endDate);
+    }
+}
--- obj/Debug/net10.0/server.AssemblyInfo.cs
+++ obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c354dc99bb97b8a47208e52f0261ce3fafd5b55f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+badf7d8c8d1e4367bd06e97f561c91cc655f9c14")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
